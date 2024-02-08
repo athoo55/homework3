10eefb0a8bf78c197a3c31b6bf5c2f6eab0fa47f
athoo55 <dhwdhwra@gmail.com>
2024-02-09 00:36:54 +0300

    add homwork

---
+++ index.js
@@ -0,0 +1,73 @@
+// Task 1
+// var myAge =18;
+// if(myAge<=20){
+//   console.log('you are eligible for voting');
+// }
+// else{
+//   console.log('you are not eligible for voting');
+// }
+
+//Task 2
+
+
+// var i = 5;
+// while (i=1) {
+//   console.log(i);
+//   i++;
+// }
+
+// // Task 3
+// function printEvennumbers(){
+//   for( var i=0;i<=10;i+=2)
+
+//   {console.log(i);}
+// }
+// printEvennumbers()
+
+// Task 4
+
+// var day = 7;
+// switch (day) {
+//   case 1:
+//     console.log('Saturday');
+//     break;
+//     case 2:
+//       console.log('Sunday');
+    
+//       break;
+//       case 3:
+//       console.log('Monday');
+    
+//       break;
+//       case 4:
+//       console.log('Tuesday');
+    
+//       break;
+//       case 5:
+//       console.log('Wednesday');
+    
+//       break;
+//       case 6:
+//       console.log('Thursday');
+    
+//       break;
+//       case 7:
+//        console.log('Friday');
+  
+//     break;
+//      default:
+//     console.log('put the correct number');
+//     break;
+// }
+// Task 5
+
+
+// function sumTwo(num1, num2) {
+//   console.log(1 + 7);
+
+// }
+// sumTwo(1, 7)
+
+// Task 6
+// const Frout =['Apple','orange','Pear','Lemon']
+// console.log(Frout);
